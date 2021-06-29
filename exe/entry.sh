33685f51c4b525ed2fcf68faf7fd19aa0c4ee996
Joel Korpela <syright@gmail.com>
2021-06-29 08:11:36 -0700

    double square brackets or something

--- exe/entry.sh
+++ exe/entry.sh
@@ -3,17 +3,17 @@ cd /app/exe
 
 # only start if ES isn't already running
 host="http://localhost:9200"
-response=$(curl $host)
+response=$(curl --write-out %{http_code} --silent --output /dev/null "$host")
 
 >&2 echo "Checking Elasticsearch status..."
 >&2 echo "ES Status:"
 >&2 echo "$response"
 
-if [ "$response" -ne "200" ]; then
-    response=$(curl --write-out %{http_code} --silent --output /dev/null "$host")
+if [[ "$response" -ne "200" ]]; then
     >&2 echo "Elasticsearch is not running. Starting..."
 
     elasticsearch -d -p PID
+
     status=$?
     if [ $status -ne 0 ]; then
       echo "Failed to start elasticsearch: $status"
@@ -24,6 +24,7 @@ fi
 >&2 echo "Starting server..."
 
 ruby elastic_ruby_server
+
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start elastic_ruby_server: $status"
