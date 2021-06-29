4ea512b78091f17b4e89099a927e8d012651aae2
Joel Korpela <syright@gmail.com>
2021-06-29 07:46:48 -0700

    prevent elasticsearch from starting up multiple times

--- exe/entry.sh
+++ exe/entry.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 cd /app/exe
 
-elasticsearch -d -p PID
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start elasticsearch: $status"
-  exit $status
-fi
+# only start if ES isn't already running
+host="http://localhost:9200"
+response=$(curl $host)
+
+>&2 echo "Checking Elasticsearch status..."
+
+until [ "$response" = "200" ]; do
+    response=$(curl --write-out %{http_code} --silent --output /dev/null "$host")
+    >&2 echo "Elasticsearch is not running. Starting..."
+
+    elasticsearch -d -p PID
+    status=$?
+    if [ $status -ne 0 ]; then
+      echo "Failed to start elasticsearch: $status"
+      exit $status
+    fi
+done
+
+>&2 echo "Starting server..."
 
 ruby elastic_ruby_server
 status=$?
